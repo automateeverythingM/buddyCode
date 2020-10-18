8920f29eea096d1da29be95730ec5e30c1f2fc4f
automateeverythingM <markospavicbg@gmail.com>
2020-10-18 18:18:59 +0200

    vracen link za taglist

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
@@ -99,6 +100,7 @@
 
     
     <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/mine.js') }}"></script>
     @yield('scripts')
 </body>
 </html>
