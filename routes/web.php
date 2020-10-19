df985a454ccb105ec1d1a1e995be5f395e0f3198
djolep <djolep987@gmail.com>
2020-10-19 11:24:44 +0200

    add profile page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+
+use App\Http\Controllers\PageController;
+
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +20,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/profile', [PageController::class, 'profile']);
 
 Auth::routes();
 
